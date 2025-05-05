49185e2e2d1274fdbbdd579fefb37e6d7a3c0225
Angel Ramirez <aramirezd@gmail.com>
2025-05-04 20:16:24 -0600

    Anotacion Primary

---
+++ src/main/java/com/angel/springboot/di/app/springboot_di/repositories/ProductRepositoryFooImpl.java
@@ -0,0 +1,26 @@
+package com.angel.springboot.di.app.springboot_di.repositories;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.angel.springboot.di.app.springboot_di.models.Product;
+
+@Repository
+public class ProductRepositoryFooImpl implements IProductRepository{
+
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(new Product(1l, "Monitor ASUS 27", 600L));
+    }
+
+    @Override
+    public Product findById(Long id) {
+        
+        return new Product(id, "Monitor ASUS 27", 600L);
+    }
+
+    
+
+}
